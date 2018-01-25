bf229b3d54722fed778f99d1a3b65c35e1a1d533
wesleysousalacerda <wesleysousalacerda@gmail.com>
2018-01-25 20:12:00 -0200

    Eventos Restful
    Testados

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
 	var EventsController = {
 		listEvents : function(request, response){
-			var sql = 'SELECT `id_estabelecimento`, `nome_evento`, `data`, `descricao`,`horario` FROM tb_eventos WHERE dismist=0';
+			var sql = 'SELECT `id_evento`, `id_estabelecimento`, `nome_evento`, `data`, `descricao`,`horario` FROM tb_eventos WHERE dismist=0';
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
@@ -22,10 +22,27 @@ module.exports = function(app) {
 			});
 		},
 		updateEvents : function(request,response){
-			
+			var eventId = request.params.eventId;
+			var event = request.body;
+			var sql = 'UPDATE `tb_eventos` SET `id_estabelecimento`="'+event.id_estabelecimento+'", `nome_evento`="'+event.nome_evento+'", `descricao`="'+event.descricao+'", `data`="'+event.data+'" , `horario`="'+event.horario+'", `banner`="'+event.banner+'" WHERE `id_evento` = "'+eventId+'"';
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});
 		},
 		deleteEvents : function(request,response){
-			
+			var eventId = request.params.eventId;
+			var sql = 'UPDATE `tb_eventos` SET `dismist`= 1 WHERE `id_evento` = "'+eventId+'"';
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});
 		}
 	};
 	return EventsController;
--- app/routes/events.js
+++ app/routes/events.js
@@ -2,6 +2,6 @@ module.exports = function(app){
 	var events = app.controllers.events;
 	app.get('/api/events',events.listEvents);
 	app.post('/api/events',events.saveEvents);
-	app.put('/api/events',events.updateEvents);
-	app.delete('/api/events',events.deleteEvents);
+	app.put('/api/events/:eventId',events.updateEvents);
+	app.delete('/api/events/:eventId',events.deleteEvents);
 }
