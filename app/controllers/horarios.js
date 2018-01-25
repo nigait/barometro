3225dd22585e9b333c20efe4a7dda83623e8841a
wesleysousalacerda <wesleysousalacerda@gmail.com>
2018-01-25 19:59:35 -0200

    Horarios Restful
    Testados

--- app/controllers/horarios.js
+++ app/controllers/horarios.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
 	var HorariosController = {
 		listHorarios : function(request,response){
-			var sql = 'SELECT `nome`, `telefone`, `usuario` FROM `tb_usuarios` WHERE dismist = 0'; 
+			var sql = 'SELECT `id_horario`,`id_estabelecimento`, `dia_inicio`, `dia_final`, `hora_inicio`, `hora_final` FROM `tb_horarios` WHERE dismist = 0'; 
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
@@ -11,8 +11,8 @@ module.exports = function(app){
 			});
 		},
 		saveHorarios : function(request,response){
-			var user = request.body;
-			var sql = 'INSERT INTO tb_usuarios(nome, telefone, usuario, senha, controle, id_perfil, dismist) VALUES ("'+user.nome+'", "'+user.telefone+'", "'+user.usuario+'", "'+user.senha+'", "'+user.controle+'", 3,0)';
+			var horario = request.body;
+			var sql = 'INSERT INTO tb_horarios(id_estabelecimento, dia_inicio, dia_final, hora_inicio, hora_final, dismist) VALUES ("'+horario.id_estabelecimento+'", "'+horario.dia_inicio+'", "'+horario.dia_final+'", "'+horario.hora_inicio+'", "'+horario.hora_final+'",0)';
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
@@ -22,13 +22,27 @@ module.exports = function(app){
 			});
 		},
 		updateHorarios : function(request,response){
-			
+			var horarioId = request.params.horarioId;
+			var horario = request.body;
+			var sql = 'UPDATE `tb_horarios` SET `id_estabelecimento`="'+horario.id_estabelecimento+'" , `dia_inicio`="'+horario.dia_inicio+'", `dia_final`="'+horario.dia_final+'", `hora_inicio`="'+horario.hora_inicio+'", `hora_final`="'+horario.hora_final+'" WHERE `id_horario` = "'+horarioId+'"';
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});
 		},
 		deleteHorarios : function(request,response){
-			
-		},
-		login : function(request,response){
-			
+			var horarioId = request.params.horarioId;
+			var sql = 'UPDATE `tb_horarios` SET `dismist`= 1 WHERE `id_horario` = "'+horarioId+'"';
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});
 		}
 
 	};
--- app/routes/horarios.js
+++ app/routes/horarios.js
@@ -2,6 +2,6 @@ module.exports = function(app){
 	var horarios = app.controllers.horarios;
 	app.get('/api/horarios',horarios.listHorarios);
 	app.post('/api/horarios',horarios.saveHorarios);
-	app.put('/api/horarios',horarios.updateHorarios);
-	app.delete('/api/horarios',horarios.deleteHorarios);
+	app.put('/api/horarios/:horarioId',horarios.updateHorarios);
+	app.delete('/api/horarios/:horarioId',horarios.deleteHorarios);
 }
