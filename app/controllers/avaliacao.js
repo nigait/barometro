5ad7135baad4c3f6260e1fb0b18cbcbd96c5d6f1
wesleysousalacerda <wesleysousalacerda@gmail.com>
2018-01-26 01:24:35 -0200

    Avaliacao RestFul
    Testado com Postmon

--- app/controllers/avaliacao.js
+++ app/controllers/avaliacao.js
@@ -1,7 +1,9 @@
 module.exports = function(app) {
 	var AvaliacaoController = {
 		listAvaliacao : function(request, response){
-			var sql = 'SELECT `id_usuario_app`, `tipo_avaliacao`, `voto`, `id_referencia_avaliacao` FROM tb_avaliacao WHERE dismist=0';
+			var typeAval = request.params.typeAval;
+			var idRef = request.params.idRef;
+			var sql = 'SELECT `id_avaliacao`,`id_usuario_app`,`voto`, `tipo_avaliacao`, `id_referencia_avaliacao` FROM tb_avaliacao WHERE dismist=0 AND tipo_avaliacao = '+typeAval+' AND id_referencia_avaliacao = '+idRef;
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
@@ -12,7 +14,7 @@ module.exports = function(app) {
 		},
 		saveAvaliacao : function(request, response){
 			var avaliacao = request.body;
-			var sql = 'INSERT INTO tb_avaliacao(id_usuario_app, tipo_avaliacao, voto, id_referencia_avaliacao, dismist) VALUES (1,"'+avaliacao.tipo_avaliacao+'","'+avaliacao.voto+'","'+avaliacao.id_referencia_avaliacao+'",0)'
+			var sql = 'INSERT INTO tb_avaliacao(id_usuario_app,voto, tipo_avaliacao, id_referencia_avaliacao, dismist) VALUES ("'+avaliacao.id_usuario_app+'","'+avaliacao.voto+'","'+avaliacao.tipo_avaliacao+'","'+avaliacao.id_referencia_avaliacao+'",0)'
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
@@ -22,10 +24,28 @@ module.exports = function(app) {
 			});
 		},
 		updateAvaliacao : function(request,response){
-			
+			var avalId = request.params.avalId;
+			var avaliacao = request.body;
+			var sql = 'UPDATE `tb_avaliacao` SET `id_usuario_app`="'+avaliacao.id_usuario_app+'",`voto`="'+avaliacao.voto+'",`tipo_avaliacao`="'+avaliacao.tipo_avaliacao+'",`id_referencia_avaliacao`="'+avaliacao.id_referencia_avaliacao+'" WHERE  `id_avaliacao` = "'+avalId+'"';
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});	
 		},
 		deleteAvaliacao : function(request,response){
-			
+			var avalId = request.params.avalId;
+			var aval = request.body;
+			var sql = 'UPDATE `tb_avaliacao` SET `dismist`= 1 WHERE `id_avaliacao` = "'+avalId+'"';
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});
 		}
 	};
 	return AvaliacaoController;
--- app/routes/avaliacao.js
+++ app/routes/avaliacao.js
@@ -2,6 +2,6 @@ module.exports = function(app){
 	var avaliacao = app.controllers.avaliacao;
 	app.get('/api/avaliacao/:typeAval/:idRef',avaliacao.listAvaliacao);
 	app.post('/api/avaliacao',avaliacao.saveAvaliacao);
-	app.put('/api/avaliacao/:typeAval/:idRef',avaliacao.updateAvaliacao);
-	app.delete('/api/avaliacao/:typeAval/:idRef',avaliacao.deleteAvaliacao);
+	app.put('/api/avaliacao/:avalId',avaliacao.updateAvaliacao);
+	app.delete('/api/avaliacao/:avalId',avaliacao.deleteAvaliacao);
 }
