6fb788144efd26e6353c1c03d7e246abe61d8b8d
wesleysousalacerda <wesleysousalacerda@gmail.com>
2018-01-26 00:31:39 -0200

    Comentarios RestFul
    Testado com Postmon

--- app/controllers/comentarios.js
+++ app/controllers/comentarios.js
@@ -24,13 +24,32 @@ module.exports = function(app){
 			});
 		},
 		updateComentarios : function(request,response){
-			
+			var typeComment = request.params.typeComment;
+			var idRef = request.params.idRef;
+			var idComment = request.params.idComment;
+			var comment = request.body;
+			var sql = 'UPDATE `tb_comentarios` SET `id_usuario_app`="'+comment.id_usuario_app+'",`horario`="'+comment.horario+'",`texto_comentario`="'+comment.texto_comentario+'",`tipo_comentario`="'+comment.tipo_comentario+'",`id_referencia`="'+comment.id_referencia+'" WHERE `tipo_comentario` = "'+typeComment+'" AND `id_comentario` = "'+idComment+'" AND id_referencia = '+idRef;
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});	
 		},
 		deleteComentarios : function(request,response){
-			
-		},
-		login : function(request,response){
-			
+			var typeComment = request.params.typeComment;
+			var idRef = request.params.idRef;
+			var idComment = request.params.idComment;
+			var comment = request.body;
+			var sql = 'UPDATE `tb_comentarios` SET `dismist`= 1 WHERE `tipo_comentario` = "'+typeComment+'" AND `id_comentario` = "'+idComment+'" AND id_referencia = '+idRef;
+			app.get('db').query(sql, function(err,rows){
+				if (err == null){
+					response.json(rows)
+				} else {
+					response.json(err)
+				}
+			});
 		}
 
 	};
--- app/routes/comentarios.js
+++ app/routes/comentarios.js
@@ -2,6 +2,6 @@ module.exports = function(app){
 	var comentarios = app.controllers.comentarios;
 	app.get('/api/comentarios/:typeComment/:idRef',comentarios.listComentarios); // typeComment define o tipo de comentario, idRef indice referecia do local ou evento.
 	app.post('/api/comentarios',comentarios.saveComentarios);
-	app.put('/api/comentarios/:typeComment/:idRef',comentarios.updateComentarios);
-	app.delete('/api/comentarios/:typeComment/:idRef',comentarios.deleteComentarios);
+	app.put('/api/comentarios/:typeComment/:idRef/:idComment',comentarios.updateComentarios);
+	app.delete('/api/comentarios/:typeComment/:idRef/:idComment',comentarios.deleteComentarios);
 }
