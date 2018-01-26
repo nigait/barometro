0d27c214b26e852638d257d4ce10105bcfbdd11b
wesleysousalacerda <wesleysousalacerda@gmail.com>
2018-01-26 01:24:06 -0200

    Fixed Comentarios
    Correção dos metodos PUT and Delete. Nao mais necesserio especificar tipo e id referencia.

--- app/controllers/comentarios.js
+++ app/controllers/comentarios.js
@@ -24,11 +24,9 @@ module.exports = function(app){
 			});
 		},
 		updateComentarios : function(request,response){
-			var typeComment = request.params.typeComment;
-			var idRef = request.params.idRef;
-			var idComment = request.params.idComment;
+			var commentId = request.params.commentId;
 			var comment = request.body;
-			var sql = 'UPDATE `tb_comentarios` SET `id_usuario_app`="'+comment.id_usuario_app+'",`horario`="'+comment.horario+'",`texto_comentario`="'+comment.texto_comentario+'",`tipo_comentario`="'+comment.tipo_comentario+'",`id_referencia`="'+comment.id_referencia+'" WHERE `tipo_comentario` = "'+typeComment+'" AND `id_comentario` = "'+idComment+'" AND id_referencia = '+idRef;
+			var sql = 'UPDATE `tb_comentarios` SET `id_usuario_app`="'+comment.id_usuario_app+'",`horario`="'+comment.horario+'",`texto_comentario`="'+comment.texto_comentario+'",`tipo_comentario`="'+comment.tipo_comentario+'",`id_referencia`="'+comment.id_referencia+'" WHERE  `id_comentario` = "'+commentId+'"';
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
@@ -38,11 +36,9 @@ module.exports = function(app){
 			});	
 		},
 		deleteComentarios : function(request,response){
-			var typeComment = request.params.typeComment;
-			var idRef = request.params.idRef;
-			var idComment = request.params.idComment;
+			var commentId = request.params.commentId;
 			var comment = request.body;
-			var sql = 'UPDATE `tb_comentarios` SET `dismist`= 1 WHERE `tipo_comentario` = "'+typeComment+'" AND `id_comentario` = "'+idComment+'" AND id_referencia = '+idRef;
+			var sql = 'UPDATE `tb_comentarios` SET `dismist`= 1 WHERE `id_comentario` = "'+commentId+'"';
 			app.get('db').query(sql, function(err,rows){
 				if (err == null){
 					response.json(rows)
